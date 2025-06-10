7041e82929841cc7ca68172a6b629b9258087ba2
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2025-06-10 13:39:11 -0400

    blank aux_file nifti header

--- yaxil/__version__.py
+++ yaxil/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'yaxil'
 __description__ = 'Yet another XNAT interface libary'
 __url__ = 'https://github.com/harvard-nrg/yaxil'
-__version__ = '0.11.2'
+__version__ = '0.12.0'
 __author__ = 'Neuroinformatics Research Group'
 __author_email__ = 'info@neuroinfo.org'
